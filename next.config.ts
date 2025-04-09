64ededc58bca7025c3eeca86fdc68ea2950c3500
Omondi Agunga <agunga3d@gmail.com>
2025-04-09 10:52:52 +0300

    update to the env varriables file

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  env: {
-    NEXT_PUBLIC_BASE_URL: 'https://1be0-105-160-20-66.ngrok-free.app',
-    NEXT_LOCAL_BASE_URL: 'http://localhost:9090/api/',
-  },
 
   webpack(config) {
     config.module.rules.push({
